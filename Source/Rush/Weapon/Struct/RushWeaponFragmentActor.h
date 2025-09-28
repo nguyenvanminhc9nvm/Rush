2ead7223b23f11e6588717e17320216b4ca62e9a
minhnv <minhnv@kaopiz.com>
2025-09-28 23:32:54 +0700

    Update Loadout table data

--- Source/Rush/Weapon/Struct/RushWeaponFragmentActor.h
+++ Source/Rush/Weapon/Struct/RushWeaponFragmentActor.h
@@ -12,3 +12,27 @@ struct FRushFragmentWeaponActor: public FTableRowBase
 	TSubclassOf<AActor> FragmentActor;
 	
 };
+
+USTRUCT(BlueprintType)
+struct FRushFragmentWeaponMesh: public FTableRowBase
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	UStaticMesh* FragmentMesh;
+};
+
+USTRUCT()
+struct FRushFragmentWeaponIronSight: public FTableRowBase
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	UStaticMesh* FragmentMesh;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	UTexture2D* Icon;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	FDataTableRowHandle RowHandleSettingScope;
+};
